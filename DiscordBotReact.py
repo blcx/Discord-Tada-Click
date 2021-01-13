6167a5d46d6ec0b28982884dbabeede4dff9d752
coderx <36203292+blcx@users.noreply.github.com>
2021-01-13 11:51:02 +0530

    Update DiscordBotReact.py

--- DiscordBotReact.py
+++ DiscordBotReact.py
@@ -6,30 +6,33 @@ def click(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 
-def tada():
-    if pyautogui.locateOnScreen('tada.png',grayscale=True, confidence=0.9) != None:
-
-        image = pyautogui.locateOnScreen('tada.png',grayscale=True, confidence=0.9)
-
-        x, y = pyautogui.center(image)
-        pyautogui.moveTo(x, y)
-        pyautogui.click()
-    time.sleep(2)
-
-
 def clickicon():
     image = pyautogui.locateOnScreen('tada.png', grayscale=True, confidence=0.9)
     if image != None:
         x, y = pyautogui.center(image)
         pyautogui.moveTo(x, y)
-        pyautogui.click()
+        #pyautogui.click()
+
+
+def find_airdrop_panel():
+    cordinates = pyautogui.locateAllOnScreen('airdrop_panel.png',confidence=0.8)
+    for element in cordinates:
+        if element != None:
+            tada = pyautogui.locateOnScreen('tada.png', grayscale=True, confidence=0.9,
+                                            region=(element[0], element[1], element[2], element[3]))
+            if tada != None:
+                print("tada icon found")
+                pyautogui.moveTo(tada)
+                pyautogui.click()
+                print("clicked")
+                pyautogui.moveTo(1079/2,1919/2)
 
 
 
 
 Run_Times = 0
 while keyboard.is_pressed('q') != True:
-    clickicon()
+    find_airdrop_panel()
     time.sleep(1)
     Run_Times = Run_Times + 1
     print("Run Times" + str(Run_Times))
